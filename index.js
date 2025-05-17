29d1ecbd1e4dd664f2696d401c10ef9aa1e3e04d
Angad Bari <angadbari2@gmail.com>
2025-05-17 18:13:29 +0530

    add youtube link

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/',(req,res)=>{
 app.get("/twitter", (req, res) => {
   res.send("AngadBari2@gmail.com");
 });
+
+app.get("/youtube", (req, res) => {
+  res.send("Unique Rigtone 985");
+});
    
 app.listen(port,()=>{
     console.log(`Server Runing at port ${port}...` )
